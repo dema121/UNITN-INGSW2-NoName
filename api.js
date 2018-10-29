444dce0d3b1bbab82019a510d154b8645cfbbd8b
dema121 <andrea.dematte@gmail.com>
2018-10-29 17:16:26 +0100

    Created basic api server

---
+++ api.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+ 
+var members = [
+    {id: 1, name:"Demattè Andrea"},
+    {id: 2, name:"Mazzucchi Gianluca"},
+    {id: 3, name:"Meroni Giacomo"},
+    {id: 4, name:"Oniarti Alessandro"},
+    {id: 5, name:"Pisetta Alex"}
+];
+ 
+ 
+app.get('/', (req, res) => res.send('Hello World! Visit <a href="/team">/team</a> for team members'))
+ 
+app.get('/team', (req, res) => {
+   res.json(members)
+})
+ 
+app.listen(PORT, () => console.log('Example app listening on port:'+ PORT))
+
